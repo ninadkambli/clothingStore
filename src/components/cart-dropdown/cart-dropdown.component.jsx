18eb9c03e82b90c9b2d8efe96d90f538fc123d05
nin.kambli@gmail.com <nin.kambli@gmail.com>
2024-01-08 17:06:27 +0530

    cartDropdown StyleComponents

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,8 @@
-import "./cart-dropdown.styles.scss";
+import {
+  CartDropdownContainer,
+  EmptyMessage,
+  CartItems,
+} from "./cart-dropdown.styles";
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import Button from "../button/button.component";
@@ -12,14 +16,18 @@ const CartDropdown = () => {
     navigate("/checkout");
   };
   return (
-    <div className="cart-dropdown-container">
-      <div className="cart-items">
-        {cartItems.map((item) => {
-          return <CartItem key={item.id} cartItem={item} />;
-        })}
-      </div>
+    <CartDropdownContainer>
+      <CartItems>
+        {cartItems.length ? (
+          cartItems.map((item) => {
+            return <CartItem key={item.id} cartItem={item} />;
+          })
+        ) : (
+          <EmptyMessage>Your cart is empty</EmptyMessage>
+        )}
+      </CartItems>
       <Button onClick={gotoCheckout}>Go To Checkout</Button>
-    </div>
+    </CartDropdownContainer>
   );
 };
 
